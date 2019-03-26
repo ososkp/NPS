6ff15c4dde97a25320192f0235ddd0dd10ae424b
OsosKP <ososkp@gmail.com>
2019-03-26 18:50:25 +0000

    Updating gitignore

--- ParksService/ParksService/obj/Debug/netcoreapp2.1/PubTmp/Out/wwwroot/Scripts/Helpers/MapOnGoogle.js
+++
@@ -1,5 +0,0 @@
-﻿// Google Maps' search requires format "1.111111,2.222222" for coordinates
-var getUrl = function (coords) {
-    var url = "http://maps.google.com/maps?q=";
-    return url + coords.replace(/[a-zA-Z]|:|\s/g, "");
-};
