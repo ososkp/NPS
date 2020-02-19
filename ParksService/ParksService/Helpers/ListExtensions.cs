dbef4d26923fbc43ba5ecdd43531a2ca5ea5d141
OsosKP <ososkp@gmail.com>
2020-02-19 17:29:48 +0000

    Make isNullOrEmpty() function cleaner

--- ParksService/ParksService/Helpers/ListExtensions.cs
+++ ParksService/ParksService/Helpers/ListExtensions.cs
@@ -5,18 +5,9 @@ namespace ParksService.Helpers
 {
     public static class ListExtensions
     {
-	 	// Checks if an IEnumerable is null or empty
 		public static bool IsNullOrEmpty<T>(this IEnumerable<T> enumerable)
 	    {
-		    switch (enumerable)
-		    {
-			    case null:
-				    return true;
-			    case ICollection<T> collection:
-				    return collection.Count < 1;
-		    }
-
-		    return !enumerable.Any();
+			return enumerable == null || !enumerable.Any();
 	    }
     }
 }
